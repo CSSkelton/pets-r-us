146a329959b7464106e6f2307169fb81d1d2e863
CSSkelton <codysskelton@gmail.com>
2023-12-11 02:20:48 -0500

    week-6 assignment

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const logger = require('pino')();
 const path = require('path');
+const Customer = require('./models/customer');
+
+//DB Connection
+var mongoose = require('mongoose');
+const conn = "mongodb+srv://codysskelton:K87m9B8CjIHIoVe2@cluster0.ifz0to0.mongodb.net/web340DB?retryWrites=true&w=majority"
+
+mongoose.connect(conn).then(() => {
+    console.log("Connection to the database was successful");
+}).catch(err => {
+    console.log("MongoDB Error: " + err.message);
+})
 
 const app = express();
 
@@ -10,6 +21,9 @@ app.use('/img', express.static(__dirname + 'public/img'));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
@@ -35,6 +49,38 @@ app.get('/training', (req, res) => {
     res.render('training', {
         title: 'Pets-R-Us Training Services'
     });
+})
+
+app.get('/register', (req, res) => {
+    res.render('register', {
+        title: 'Pets-R-Us Client Registration'
+    })
+})
+
+app.post('/registration', async (req, res, next) => {
+    console.log(req.body);
+    console.log(req.body.clientName);
+    console.log(req.body.clientEmail);
+    const newCustomer = new Customer({
+        customerId: req.body.clientName,
+        email: req.body.clientEmail
+    })
+
+    console.log(newCustomer);
+
+    // Resulting Error: MongooseError: Model.create() no longer accepts a callback
+    /*Customer.create(newCustomer, function(err, customer) {
+        if (err) {
+            console.log(err);
+            next(err);
+        } else {
+            res.render('index', {
+                title: 'Pets-R-Us Home'
+            })
+        }
+    })*/
+    //The following code works, but I'm unsure of how to log the results and reroute afterwards.
+    await Customer.create({ customerId: req.body.clientName, email: req.body.clientEmail });
 })
 
 app.listen(PORT, () => {
